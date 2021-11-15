aa69504f7052645c3a469c5e6e0c42cdd1d4dd6e
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-11-15 10:44:34 +0100

    Update ZuulServerApplication.java

--- src/main/java/com/example/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/example/zuulserver/ZuulServerApplication.java
@@ -3,11 +3,12 @@ package com.example.zuulserver;
 import com.github.mthizo247.cloud.netflix.zuul.web.socket.EnableZuulWebSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulWebSocket
 @SpringBootApplication
 public class ZuulServerApplication {
