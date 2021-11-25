7e69571bf40a122dded6ee6986baead1f6cc0a58
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-11-25 05:47:46 +0100

    Added specific cors configuration and fixed routes

--- src/main/java/com/example/gateway/config/WebSecurityConfig.java
+++ src/main/java/com/example/gateway/config/WebSecurityConfig.java
@@ -3,12 +3,18 @@ package com.example.gateway.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebFluxSecurity
 public class WebSecurityConfig {
@@ -21,16 +27,30 @@ public class WebSecurityConfig {
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.csrf().disable()
-                .authorizeExchange()
-                .pathMatchers(HttpMethod.GET, "/api/v1/dishes").permitAll()
+        http.authorizeExchange()
+                .pathMatchers(HttpMethod.GET, "/api/v1/dish").permitAll()
                 .pathMatchers(HttpMethod.GET, "/api/v1/categories").permitAll()
                 .pathMatchers(HttpMethod.GET, "/api/v1/ingredient").permitAll()
                 .pathMatchers(HttpMethod.GET, "/register").permitAll()
                 .anyExchange().authenticated()
+                .and().cors().configurationSource(corsConfigurationSource())
                 .and().oauth2ResourceServer()
                 .jwt().jwtDecoder(jwtDecoder());
         return http.build();
+    }
+
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(List.of(
+                HttpMethod.GET.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.DELETE.name()
+        ));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
+        return source;
     }
 
     ReactiveJwtDecoder jwtDecoder() {
